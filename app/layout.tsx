7af085223d50fd420dfcfd3d7561452f69b8d195
Darko Bradas <bradasdarko1@gmail.com>
2026-06-09 11:00:24 +0200

    Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import Header from '@/components/Header'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function RootLayout({
   children,
@@ -11,6 +12,7 @@ export default function RootLayout({
       <body className="bg-[#FAFAF7] text-[#222222] pb-16 lg:pb-0">
         <Header />
         {children}
+        <Analytics />
       </body>
     </html>
   )
